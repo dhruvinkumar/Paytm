dfb5bf3214d4807a7ef4565fe1903bd241bd386f
DHRUVINKUMAR BHATT <75667686+dhruvinkumar@users.noreply.github.com>
2024-07-28 00:21:00 +0530

    Add files via upload

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier", // Ensure this is last to override other configs
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "18.2",
+    },
+  },
+  plugins: ["react-refresh", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": 0,
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "prettier/prettier": 0,
+    "react/prop-types": "off" // Disable prop-types validation
+  },
+};
